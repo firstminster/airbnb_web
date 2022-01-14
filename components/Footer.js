5d6d78056caff9e91c74f247d61467db8ceb9942
Chinecherem Harry <chiharry@firstmirMacBook.lan>
2022-01-14 12:01:11 +0100

    footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600  sm:place-items-center ">
+    <div className=" grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 md:place-items-center  ">
+      {/* <div className="sm:place-items-center">
+        
+      </div> */}
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
         <p>How Airbnb works</p>
